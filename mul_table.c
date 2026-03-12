7e08ab39233be84a571984104919a57a82be36f1
Karan Ray <karanray06@users.noreply.github.com>
2026-03-12 20:46:37 +0530

    Initial commit

---
+++ mul_table.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, num;
+    printf("Enter a number:");
+    scanf("%d", &num);
+//    printf("Multiplication table of %d is:\n", num);
+    for(i=1; i<=10; i++)
+    {
+        printf("%d x %d = %d\n", num, i, num*i);
+    }
+    return 0;
+}
